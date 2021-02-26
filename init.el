bfeb83cf69a993bb6aa0bd59c902623e67113622
Kevin Ziegler <ziegler.kevin@heb.com>
2021-02-26 13:54:19 -0600

    Add JSON module

--- init.el
+++ init.el
@@ -126,6 +126,7 @@
        ;;(haskell +dante)  ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ;
+       (json +lsp)
        (java +lsp) ; the poster child for carpal tunnel syndrome
        (javascript +lsp)        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
