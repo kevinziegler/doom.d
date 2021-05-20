de1ac52ea668510c72179ba5c86fa256a83fa785
Kevin Ziegler <ziegler.kevin@heb.com>
2021-05-20 15:35:24 -0500

    Switch font from Fira to Iosevka

--- conf.d/ui.el
+++ conf.d/ui.el
@@ -7,9 +7,12 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "FiraCode Nerd Font" :size 13)
-      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13)
-      doom-big-font (font-spec :family "Fira Sans" :size 16))
+;; (setq doom-font (font-spec :family "FiraCode Nerd Font" :size 13)
+;;       doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13)
+;;       doom-big-font (font-spec :family "Fira Sans" :size 16))
+(setq doom-font (font-spec :family "Iosevka Nerd Font" :size 13)
+      doom-variable-pitch-font (font-spec :family "Iosevka" :size 13)
+      doom-big-font (font-spec :family "Iosevka" :size 16))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
