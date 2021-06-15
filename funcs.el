764531d838aae7a6aaf7f491c666e1752aaf499d
Kevin Ziegler <ziegler.kevin@heb.com>
2021-06-15 14:50:22 -0500

    Add function for handling Jira credentials
    This is a helper to read my Jira creds out of a netrc file and then
    translate it to the base64-encoded string that can be used in the header
    of a request to their API.

--- funcs.el
+++ funcs.el
@@ -11,3 +11,12 @@
 (defun kdz/set-zshrc-sh-shell ()
   (when (string-match "zshrc$" buffer-file-name)
     (sh-set-shell "zsh")))
+
+(defun http-basic-auth-token-b64 (host)
+  "Find the first result for the supplied HOST in auth sources (or nil if no
+   matching host is found) and return an HTTP basic auth header with a base64
+   encoded token value"
+  (let* ((credentials (nth 0 (auth-source-search :host host)))
+         (user (plist-get credentials :user))
+         (password (funcall (plist-get credentials :secret))))
+    (base64-encode-string (concat user ":" password))))
