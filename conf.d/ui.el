6c162a5239deae3e848fa5ff2874630b23fdf5e8
Kevin Ziegler <ziegler.kevin@heb.com>
2021-08-02 13:07:39 -0500

    One more theme tweak

--- conf.d/ui.el
+++ conf.d/ui.el
@@ -18,7 +18,8 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. These are the defaults.
 ;;(setq doom-theme 'doom-oceanic-next)
-(setq doom-theme 'kaolin-galaxy)
+(setq 'doom-monokai-machine)
+;;(setq doom-theme 'kaolin-galaxy)
 ;;(setq doom-theme 'kaolin-bubblegum)
 ;;(setq doom-theme 'doom-old-hope)
 ;;(setq doom-theme 'doom-1337)
