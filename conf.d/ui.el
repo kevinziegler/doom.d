348c37a665d12385c0bd3ac831f1044726e483de
Kevin Ziegler <kevin@bestow.com>
2021-09-24 16:26:48 -0500

    Add icon mappings for kaolin themes
    In particular, this fills out a lot of the lsp-treemacs cases I'm
    running into.

--- conf.d/ui.el
+++ conf.d/ui.el
@@ -54,6 +54,85 @@
 (after! treemacs
   (setq treemacs-collapse-dirs 7)
   (setq treemacs-width 45)
-  (kaolin-treemacs-theme))
+  (setq treemacs-project-follow-mode t)
+  (setq lsp-treemacs-theme 'kaolin)
+  (kaolin-treemacs-theme)
+  (treemacs-modify-theme "kaolin"
+    :config
+    (progn
+      (treemacs-create-icon
+       :icon (format " %s " (all-the-icons-fileicon "jsonld" :size 0.9))
+       :extensions ("json")
+       :fallback 'same-as-icon)
+      (treemacs-create-icon
+       :icon (format " %s " (all-the-icons-faicon "table" :size 0.9))
+       :extensions ("csv")
+       :fallback 'same-as-icon)
+      (treemacs-create-icon
+       :icon (format " %s " (all-the-icons-faicon "cogs" :size 0.9))
+       :extensions ("editorconfig" "envrc" "envrc.local")
+       :fallback 'same-as-icon)
+      (treemacs-create-icon
+       :icon (format "%s " (all-the-icons-material "class"
+                                                   :size 0.9
+                                                   :face 'font-lock-keyword-face))
+       :extensions (class)
+       :fallback 'same-as-icon)
+
+      (treemacs-create-icon
+       :icon (format "%s " (all-the-icons-material "code"
+                                                   :size 0.9
+                                                   :face 'font-lock-keyword-face))
+       :extensions (def)
+       :fallback 'same-as-icon)
+      (treemacs-create-icon
+       :icon (format "%s " (all-the-icons-material "code"
+                                                   :size 0.9
+                                                   :face 'font-lock-keyword-face))
+       :extensions (function)
+       :fallback 'same-as-icon)
+      (treemacs-create-icon
+       :icon (format "%s " (all-the-icons-material "code"
+                                                   :size 0.9
+                                                   :face 'font-lock-keyword-face))
+       :extensions (method)
+       :fallback 'same-as-icon)
+      (treemacs-create-icon
+       :icon (format "%s " (all-the-icons-faicon "tags"
+                                                   :size 0.9
+                                                   :face 'font-lock-keyword-face))
+       :extensions (enum)
+       :fallback 'same-as-icon)
+      (treemacs-create-icon
+       :icon (format "%s " (all-the-icons-faicon "tags"
+                                                   :size 0.9
+                                                   :face 'font-lock-keyword-face))
+       :extensions (enumerator)
+       :fallback 'same-as-icon)
+      (treemacs-create-icon
+       :icon (format "%s " (all-the-icons-faicon "tag"
+                                                   :size 0.9
+                                                   :face 'font-lock-keyword-face))
+       :extensions (enum-item)
+       :fallback 'same-as-icon)
+      (treemacs-create-icon
+       :icon (format "%s " (all-the-icons-faicon "tag"
+                                                   :size 0.9
+                                                   :face 'font-lock-keyword-face))
+       :extensions (enum-member)
+       :fallback 'same-as-icon)
+      (treemacs-create-icon
+       :icon (format "%s " (all-the-icons-material "info_outline"
+                                                   :size 0.9
+                                                   :face 'font-lock-keyword-face))
+       :extensions (property)
+       :fallback 'same-as-icon)
+      (treemacs-create-icon
+       :icon (format "%s " (all-the-icons-material "info_outline"
+                                                   :size 0.9
+                                                   :face 'font-lock-keyword-face))
+       :extensions (constant)
+       :fallback 'same-as-icon))))
+
 
 (after! vterm (setq vterm-shell (brew-bin "zsh")))
