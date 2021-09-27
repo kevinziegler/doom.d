1cf754eb3bcf2aadc7973ff92c9142f4cb278db0
Kevin Ziegler <kevin@bestow.com>
2021-09-27 12:26:44 -0500

    Use mypy instead of pyright
    This seems to work better with the current projects I'm developing
    for.

--- init.el
+++ init.el
@@ -154,9 +154,8 @@
        plantuml            ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
        (python             ; beautiful is better than ugly
-        +lsp
         +pyenv
-        +pyright)
+        +lsp)
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        rest                ; Emacs as a REST client
