1ebbd690bc5bf4689bf7ad8a12c0b377fcba1a5c
Kevin Ziegler <kevin@bestow.com>
2021-10-04 14:21:57 -0500

    Enable headerline breadcrumbs with LSP

--- conf.d/lsp.el
+++ conf.d/lsp.el
@@ -3,4 +3,6 @@
 (add-hook! lsp-mode #'lsp-enable-which-key-integration)
 
 (after! lsp-mode
+  (setq lsp-headerline-breadcrumb-enable t)
+  (setq lsp-headerline-breadcrumb-segments '(symbols))
   (setq lsp-idle-delay 0.8))
