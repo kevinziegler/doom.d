6a7f26c30c62b8658bb31e32da14b2e860f9f542
Kevin Ziegler <kevin@bestow.com>
2022-03-07 11:30:00 -0600

    Add some more helper packages

--- packages.el
+++ packages.el
@@ -76,3 +76,7 @@
   :recipe (:host github :repo "org-roam/org-roam-ui" :files ("*.el" "out"))
   :pin "cd1aefd56f648d32a25aae672ac1ab90893c0133")
 (package! websocket :pin "fda4455333309545c0787a79d73c19ddbeb57980") ; dependency of `org-roam-ui'
+
+(package! vertico-posframe)
+(package! topspace)
+(package! mermaid-mode)
