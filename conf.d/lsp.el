a8af0e3613080d337d8cd4934d05092371f0227f
Kevin Ziegler <kziegler@ojolabs.com>
2022-07-15 13:01:38 -0500

    Use ASDF-managed python in LSP-pyright
    The builtin lookup function in `lsp-pyright` isn't aware of ASDF shims,
    so when it passes a `python` binary to the pyright server, the server
    executes the shim somewhere in `~/.emacs.d`, where the shim points to
    the wrong python version.
    
    This helper short-circuits the python lookup and replaces it with the
    result of `asdf which python` (which gives an unshimmed path) if
    applicable.

--- conf.d/lsp.el
+++ conf.d/lsp.el
@@ -6,3 +6,22 @@
   (setq lsp-headerline-breadcrumb-enable t)
   (setq lsp-headerline-breadcrumb-segments '(symbols))
   (setq lsp-idle-delay 0.8))
+
+(setq lsp-pyright-multi-root nil)
+
+(defun kdz/asdf-which (bin)
+  (with-temp-buffer
+    (let ((asdf-lookup
+           (list (call-process "asdf" nil (current-buffer) nil "which" bin)
+                 (substring (buffer-string) 0 -1))))
+      (if (eq 0 (pop asdf-lookup)) (pop asdf-lookup)))))
+
+(defun kdz/pyright-prefer-asdf-python (origin-fn &rest args)
+  (or (kdz/asdf-which "python") (apply origin-fn args)))
+
+;; When using lsp-pyright, check if we have an asdf-managed python and prefer
+;; that when specifying a python binary for the LSP server.  This addresses
+;; issues where the pyright server process picks up the wrong python version
+;; because it runs the ASDF shim in a directory that isn't the project root
+(advice-add 'lsp-pyright-locate-python :around #'kdz/pyright-prefer-asdf-python)
+
