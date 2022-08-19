d370848add9abc1b604f96f735222efa501c1447
Kevin Ziegler <kziegler@ojolabs.com>
2022-08-19 15:43:51 -0500

    Clean up org-mode key mappings

--- conf.d/org/keybinds.el
+++ conf.d/org/keybinds.el
@@ -5,6 +5,7 @@
 
   (map! :map org-mode-map
         :localleader
+        :desc "Outline" "O" #'org-ol-tree
         (:prefix ("s" . "Subtrees")
         :desc "Cut subtree" "d" #'org-cut-subtree
         :desc "Promote subtree" "h" #'org-promote-subtree
@@ -20,9 +21,7 @@
          :desc "Heading" "h" #'org-insert-heading
          :desc "Parent Heading" "H" #'kdz/org-insert-heading-up
          :desc "Subheading" "s" #'org-insert-subheading
-         :desc "Link" "l" #'org-insert-link))
-
-  (map! :map org-mode-map
-        :after org
-        :localleader
-        :desc "Outline" "O" #'org-ol-tree))
+         :desc "Link" "l" #'org-insert-link)
+        (:prefix ("ia" . "Application Links")
+         :desc "Active Firefox Tab" "f" #'org-mac-link-firefox-insert-frontmost-url
+         :desc "Finder Item" "F" #'org-mac-link-finder-insert-selected)))
