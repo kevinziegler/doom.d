fd9b64eebfa7e62dda1ba7bc87cc78d7bf7b7f41
Kevin Ziegler <kziegler@ojolabs.com>
2022-10-21 14:31:11 -0500

    Add standalone lib keybinding helper functions

---
+++ lib/keybind-helpers.el
@@ -0,0 +1,32 @@
+
+(defun kdz/toggle-buffer-window (buffer-name activate-fn)
+  (let ((window (get-buffer-window buffer-name)))
+    (if window (delete-window window) (funcall activate-fn))))
+
+(defun kdz/toggle-lsp-symbols ()
+  (interactive)
+  (kdz/toggle-buffer-window
+   lsp-treemacs-symbols-buffer-name
+   'lsp-treemacs-symbols))
+
+(defun kdz/toggle-lsp-errors-list ()
+  (interactive)
+  (kdz/toggle-buffer-window
+   lsp-treemacs-errors-buffer-name
+   'lsp-treemacs-errors-list))
+
+(defun switch-to-message-buffer ()
+  (interactive)
+  (pop-to-buffer "*Messages*"))
+
+(evil-define-command evil-buffer-org-new (count file)
+  "Creates a new ORG buffer replacing the current window, optionally
+     editing a certain FILE"
+  :repeat nil
+  (interactive "P<f>")
+  (if file
+      (evil-edit file)
+    (let ((buffer (generate-new-buffer "*new org*")))
+      (set-window-buffer nil buffer)
+      (with-current-buffer buffer
+        (org-mode)))))
