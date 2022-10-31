582a01ada6192d823de1a2a81149049a734c2f32
Kevin Ziegler <kziegler@ojolabs.com>
2022-10-31 11:31:03 -0500

    Add configuration for `markdown-xwidget`

--- config.el
+++ config.el
@@ -116,3 +116,12 @@
 ;; (bookmark-load (concat doom-private-dir "bookmarks"))
 
 (setq ein:polymode t)
+
+;; TODO Set popup rules for Markdown Xwidget to use right/50% split for the
+;;      preview buffer
+;; TODO Figure out "format-all-errors" when running preview mode
+(after! markdown-xwidget
+  (setq markdown-xwidget-command "pandoc"
+        markdown-xwidget-github-theme "light"
+        markdown-xwidget-mermaid-theme "default"
+        markdown-xwidget-code-block-theme "default"))
