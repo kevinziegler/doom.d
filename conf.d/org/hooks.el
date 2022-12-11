f49463884909be6ffa4c2af0621971e9ab2ccac9
Kevin Ziegler <kevinziegler@Balder.asgard>
2022-12-11 11:11:46 -0600

    Auto-save in org-mode buffers

--- conf.d/org/hooks.el
+++ conf.d/org/hooks.el
@@ -10,4 +10,5 @@
   (add-hook 'org-mode-hook #'org-modern-mode)
   (add-hook 'org-mode-hook (display-fill-column-indicator-mode -1))
   (add-hook 'org-mode-hook #'valign-mode)
+  (add-hook 'org-mode-hook (auto-save-mode t))
   (add-transient-hook! #'org-babel-execute-src-block (require 'ob-async)))
