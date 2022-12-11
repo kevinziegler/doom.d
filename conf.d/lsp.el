bb974e5ca0ad4be807cc9a752f30e8e468751622
Kevin Ziegler <kevinziegler@Balder.asgard>
2022-12-11 10:36:36 -0600

    Consolidate LSP configuration

--- conf.d/lsp.el
+++ conf.d/lsp.el
@@ -1,12 +1,25 @@
 ;;; config-lsp.el -*- lexical-binding: t; -*-
 
 (after! lsp-mode
-  (setq lsp-headerline-breadcrumb-enable t)
-  (setq lsp-headerline-breadcrumb-segments '(symbols))
-  (setq lsp-idle-delay 0.8)
+  (setq lsp-headerline-breadcrumb-enable t
+        lsp-headerline-breadcrumb-segments '(symbols)
+        lsp-pyright-multi-root nil
+        lsp-idle-delay 0.8)
   (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\__pycache__\\'"))
 
-(setq lsp-pyright-multi-root nil)
+(after! lsp-java
+  ;; (require 'lsp-java-boot)
+  ;; (add-hook 'lsp-mode-hook #'lsp-lens-mode)
+  ;; (add-hook 'java-mode-hook #'lsp-java-boot-lens-mode)
+  (setq lsp-java-maven-download-sources t
+        lsp-java-import-maven-enabled t)
+  (setq lombok-jar-path
+        (expand-file-name
+         "~/.m2/repository/org/projectlombok/lombok/1.18.16/lombok-1.18.16.jar"))
+  (setq lsp-java-lombok-args
+    (list (concat "-javaagent:" lombok-jar-path)))
+  (when (boundp 'lsp-java-vmargs)
+    (setq lsp-java-vmargs (append lsp-java-vmargs lsp-java-lombok-args))))
 
 (defun kdz/pyright-prefer-asdf-python (origin-fn &rest args)
   (or (kdz/asdf-which "python") (apply origin-fn args)))
