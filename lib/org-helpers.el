d823297c70cf93cfeca2999970586475e72ad8db
Kevin Ziegler <kziegler@ojolabs.com>
2023-02-13 11:17:07 -0600

    Remove unused function

--- lib/org-helpers.el
+++ lib/org-helpers.el
@@ -7,7 +7,3 @@
        (subst-char-in-string ?_ ?  buffer-file-name))
     (funcall orig-fun)))
 
-(defun +org-export-remove-zero-width-space (text _backend _info)
-  "Remove zero width spaces from TEXT."
-  (unless (org-export-derived-backend-p 'org)
-    (replace-regexp-in-string "\u200B" "" text)))
