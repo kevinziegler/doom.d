2272c3d7e7148e44be97958ed140a0238cb16264
Kevin Ziegler <kevinziegler@Balder.asgard>
2023-03-25 10:57:07 -0500

    Remove scad-preview package
    After experimenting with this, I'm realizing that using OpenSCAD's own
    window alongside Emacs and allowing it to live-update is the more
    ergonomic workflow.

--- packages.el
+++ packages.el
@@ -75,11 +75,6 @@
 (package! which-key-posframe)
 (package! wordel)
 
-;; NOTE This needs an 'openscad' binary in emac's path, but the Homebrew cask
-;;      does not install such a binary.  This can be fixed by linking 'openscad'
-;;      in '$HOME/.bin' to the installed 'OpenSCAD.app'
-(package! scad-preview)
-
 (package! markdown-xwidget
   :recipe (:host github
            :repo "cfclrk/markdown-xwidget"
