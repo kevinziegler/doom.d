4ba3ae94f6e546cf640d51d33a5be428431a7b7d
Kevin Ziegler <kziegler@ojolabs.com>
2023-04-19 12:17:47 -0500

    Clean up face tweaks for org-mode

--- conf.d/org.el
+++ conf.d/org.el
@@ -50,21 +50,6 @@
                                         (:tangle . "no")
                                         (:comments . "link")))
 
-  (set-fontset-font t 'unicode (font-spec :family "FontAwesome") nil 'prepend)
-  (set-fontset-font t 'unicode (font-spec :family "Material Icons") nil 'prepend)
-  (set-fontset-font t 'unicode (font-spec :family "github-octicons") nil 'prepend)
-  (set-face-attribute 'org-table nil :inherit 'fixed-pitch)
-  (custom-set-faces!
-    '(outline-1 :weight extra-bold :height 1.50 :underline t)
-    '(outline-2 :weight bold :height 1.40)
-    '(outline-3 :weight bold :height 1.30)
-    '(outline-4 :weight semi-bold :height 1.20)
-    '(outline-5 :weight semi-bold :height 1.15)
-    '(outline-6 :weight semi-bold :height 1.15)
-    '(outline-8 :weight semi-bold)
-    '(outline-9 :weight semi-bold)
-    '(org-document-title :height 1.9))
-
   (add-transient-hook! #'org-babel-execute-src-block (require 'ob-async))
 
   (add-hook 'org-mode-hook #'kdz/writing-minor-modes)
