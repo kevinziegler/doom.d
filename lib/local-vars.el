7629942efd8a933a6a78b5d0653637efde4ff937
Kevin Ziegler <kziegler@ojolabs.com>
2023-05-05 11:41:59 -0500

    Add local variable definitions
    These are variables intended for use in my local-only configuration that
    aren't defined elsewhere.

---
+++ lib/local-vars.el
@@ -0,0 +1,7 @@
+(defvar gitlab-hosts-alist '()
+  "List of hosted Gitlab instances to configure for git-link, etc")
+
+(defvar user-company nil
+  "Name of the company I'm working for on a given machine")
+(defvar user-team nil
+  "Name of the team I'm working for on a given machine")
