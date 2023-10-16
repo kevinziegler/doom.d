6c6d9583a10bbb85792706e1504dfd39e3fe38bd
Kevin Ziegler <kevin.ziegler3@gmail.com>
2023-10-16 11:55:23 -0500

    Clean up unused misc. lib code

--- lib/misc.el
+++ lib/misc.el
@@ -1,30 +1,4 @@
 ;;;###autoload
-(require 'string-inflection)
-
-(defun kdz/set-zshrc-sh-shell ()
-  (when buffer-file-name
-    (when (string-match "zshrc$" buffer-file-name)
-      (sh-set-shell "zsh"))))
-
-(defun http-basic-auth-token-b64 (host)
-  "Find the first result for the supplied HOST in auth sources (or nil if no
-   matching host is found) and return an HTTP basic auth header with a base64
-   encoded token value"
-  (let* ((credentials (nth 0 (auth-source-search :host host)))
-         (user (plist-get credentials :user))
-         (password (funcall (plist-get credentials :secret))))
-    (base64-encode-string (concat user ":" password))))
-
-(defmacro kdz/treemacs-all-the-icons (extensions iconset icon &rest icon-params)
-  `(treemacs-create-icon
-       :icon (format " %s "
-                     ,(append (list (intern (concat "all-the-icons-" iconset))
-                                    icon
-                                    :size 0.9)
-                              icon-params))
-       :extensions ,extensions
-       :fallback 'same-as-icon))
-
 (defun kdz/lsp-java-enable-lombok-support (lombok-version)
   (let* ((mvn-base "~/.m2")
          (lombok-package "org.projectlombok.lombok")
